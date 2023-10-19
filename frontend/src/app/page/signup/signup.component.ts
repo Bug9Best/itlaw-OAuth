c90f73426c386e09b45113c230371e8c346dd598
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-20 02:10:45 +0700

    update UI

--- frontend/src/app/page/signup/signup.component.ts
+++ frontend/src/app/page/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  formData: FormGroup = new FormGroup({
+    first_name: new FormControl('', [Validators.required]),
+    last_name: new FormControl('', [Validators.required]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirm_password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
+
+  constructor() { }
+
+  submit() {
+
+  }
 }

4d6572d3caa7baf46005fa5ea7097370ef412368
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-20 15:54:23 +0700

    update

--- frontend/src/app/page/signin/signin.component.ts
+++ frontend/src/app/page/signin/signin.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent {
+  constructor() { }
+
+  loginWithGoogle() {
+  }
+
+  loginWithFacebook() {
+  }
 
 }
